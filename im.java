22e159a1e93dc497f1d67f10a7b8b5f1cb02edf4
syed mohammed <mightysyed12345@gmail.com>
2026-08-02 16:42:52 +0530

    initial commit

--- im.java
+++ im.java
@@ -1,4 +1,4 @@
-public class im  {
+public class Im  {
     public String palindrome(int n)  {
         int t=n;
         int sum=0;
@@ -14,6 +14,6 @@ public class im  {
         }
     }
     public static void main(String[] args) {
-        System.out.println(new im().palindrome(121));
+        System.out.println(new Im().palindrome(121));
     }
 }
