24e4c60c617d1501b931db86c8dbce6656e6e9e8
mightysyed12345-sketch <mightysyed12345@gmail.com>
2026-09-09 19:48:00 +0530

    AirBnb clone

--- app.js
+++ app.js
@@ -11,17 +11,23 @@ const wrapAsync=require("./utils/wrapAsync.js");
 const Expresserror=require("./utils/Expresserror.js");
 const {listingSchema,reviewSchema}=require("./schema.js");
 const Review=require("./models/review.js");
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+
 
 const session=require("express-session");
 const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
+
 const sessionOptions={
     secret:"mysupersecretcode",
     resave:false,
@@ -33,16 +39,34 @@ const sessionOptions={
     },
 };
 
+//session middleware
 app.use(session(sessionOptions));
 app.use(flash());//pahle flash ayega phir routes ayegai
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     next();
 });
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+// app.get("/demouser", async(req,res)=>{
+//     let fakeUser=new User({
+//         email:"mightysyed12345@gmail.com",
+//         username:"syedmohammed"
+//     });
+//     let registeredUser=await User.register(fakeUser,"helloworld");//static method in which register(username,password,callback is our choice);
+//     res.send(registeredUser);
+// })
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 app.get("/",(req,res)=>{
     res.send("hi iam root");
 });
