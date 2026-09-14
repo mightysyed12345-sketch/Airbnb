bbdd7ae4bc024393612c80e417bf24b8f56e8407
mightysyed12345-sketch <mightysyed12345@gmail.com>
2026-09-14 18:28:35 +0530

    Airbnb clone

--- routes/listing.js
+++ routes/listing.js
@@ -6,17 +6,30 @@ const Review=require("../models/review.js");
 const {isloggedIn,isOwner,validateListing}=require("../middleware.js");
 const listingController=require("../controllers/listing.js");
 
-router.get("/",wrapAsync(listingController.index));
-//new routes 
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(
+    isloggedIn,
+    validateListing,
+    wrapAsync(listingController.createlisting)
+);
+//new route 
 router.get("/new",isloggedIn,listingController.renderNewForm);
-router.get("/:id",wrapAsync(listingController.showListing));
-router.post("/",validateListing,wrapAsync(listingController.createlisting));
+
+router
+.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isloggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updatelisting)
+)
+.delete(isloggedIn,isOwner,wrapAsync(listingController.deletelisting));
 //edit route 
-router.get("/:id/edit",isloggedIn,isOwner,wrapAsync(listingController.renderEditForm));
-const methodOverride=require("method-override");
-router.use(methodOverride("_method"));
-//edit  route 
-router.put("/:id",isloggedIn,isOwner,validateListing,wrapAsync(listingController.updatelisting));
-//delete route 
-router.delete("/:id" ,isloggedIn,isOwner,(wrapAsync(listingController.deletelisting)));
+router.get(
+    "/:id/edit",
+    isloggedIn,
+    wrapAsync(listingController.renderEditForm)
+);
 module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,21 @@ const passport=require("passport");
 const {saveredirectUrl:saveRedirectUrl}=require("../middleware.js");
 const userController=require("../controllers/users.js");
 
+router
+.route("/signup")
+.get(userController.rendersignup)
+.post(wrapAsync(userController.signup));
+router
+.route("/login")
+.get(userController.renderlogin)
+.post(saveRedirectUrl,passport.authenticate("local",{
+    failureRedirect:"/login",
+    failureFlash:true,
+    }),
+    userController.login
+);
 
-router.get("/signup",wrapAsync(userController.rendersignup));
-router.post("/signup",wrapAsync(userController.rendersignup));
-router.get("/login",userController.renderlogin);
-router.post("/login",saveRedirectUrl,passport.authenticate("local",
-{failureRedirect:'/login',failureFlash:true}),userController.login);
+router.route("/logout")
+.get(userController.logout);
 
-router.get("/logout",userController.logout);
 module.exports=router;
